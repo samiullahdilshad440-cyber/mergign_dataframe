5ea6a1cb8240c194bf6fc3d4bb7a88375bd0055c
Sami Khan <samiullahdilshad440@gmail.com>
2026-07-30 18:13:58 +0500

    concate the different dataframe with using the function concat() if we add the column which is not matching the other then default value is stored as NaN

--- pandas_numpy.py
+++ pandas_numpy.py
@@ -13,7 +13,30 @@ df = pd.DataFrame(np.random.randn(6, 2), index=hier_index, columns=semester)
 school2 = df.xs("School 2")
 classA = df.xs(("School 2", "Class A"), level= [0,1])
 class_level = df.xs("Class A", level=1)
-print(df)
-print(school2)
-print(classA)
-print(class_level)
+df1 = pd.DataFrame({'X':["X0","X1","X2","X3"],
+        'Y':["Y0","Y1","Y2","Y3"],
+        'Z':["Z0","Z1","Z2","Z3"],
+        'T':["T0","T1","T2","T3"]})
+df2 = pd.DataFrame({'X':["X4","X5","X6","X7"],
+        'Y':["Y4","Y5","Y6","Y7"],
+        'Z':["Z4","Z5","Z6","Z7"],
+        'T':["T4","T5","T2","T3"]})
+df3 = pd.DataFrame({'X':["X8","X9","X10","X11"],
+        'Y':["Y8","Y9","Y10","Y11"],
+        'Z':["Z8","Z9","Z10","Z11"],
+        'T':["T8","T9","T10","T11"]})
+concate = pd.concat([df1, df2, df3])
+concate_withignoreindex = pd.concat([df1, df2, df3], ignore_index=True)
+df2.columns = ["X","Y","Z","A"]
+df1df2 = pd.concat([df1,df2], ignore_index=True)
+# print(df)
+# print(school2)
+# print(classA)
+# print(class_level)
+# print(df1)
+# print(df2)
+# print(df3)
+print(concate)
+print(concate_withignoreindex)
+print(df2)
+print(df1df2)
