e1a2efd7fe4e12c5da4eca86620d1dc01d0e1417
Sami Khan <samiullahdilshad440@gmail.com>
2026-07-31 16:54:44 +0500

    with examples and for understanding read readme.md file

--- pandas_numpy.py
+++ pandas_numpy.py
@@ -10,6 +10,10 @@ df2 = pd.DataFrame({
     'country': ['Canada', 'England', 'USA', 'Germany'],
     'salary': [22000, 16000, 20000, 17500]
 })
+df3 = pd.DataFrame({
+    'manager': ['Abraham', 'Joseph', 'Kayra'],
+    'department': ['Web Development', 'Cyber Security', 'Data Science']
+})
 
 result = pd.merge(df1, df2,)
 
@@ -40,11 +44,49 @@ print("=" * 60)
 print(right)
 
 outer = pd.merge(df1, df2, on="employee", how="outer")
-
 print("=" * 60)
 print("Outer Merge")
 print("- Keeps all rows from both DataFrames.")
 print("- Missing matches are filled with NaN.")
 print("=" * 60)
-
 print(outer)
+
+df4 = pd.merge(df1, df2)
+print("=" * 70)
+print("Step 1 : Merge Employee Details")
+print("- Matching is performed using the 'employee' column.")
+print("=" * 70)
+print(df4)
+
+df6 = pd.DataFrame({
+    'department': [
+        'Web Development',
+        'Web Development',
+        'Cyber Security',
+        'Cyber Security',
+        'Data Science',
+        'Data Science'
+    ],
+    'prog_lang': [
+        'HTML',
+        'CSS',
+        'C++',
+        'SQL',
+        'PYTHON',
+        'R'
+    ]
+})
+
+print("=" * 70)
+print("Department and Programming Languages")
+print("- Each department can have multiple programming languages.")
+print("=" * 70)
+print(df6)
+
+result = pd.merge(df4, df6, on="department")
+print("=" * 70)
+print("Merge Employees with Programming Languages")
+print("- Matching is performed using the 'department' column.")
+print("- One department has multiple programming languages.")
+print("=" * 70)
+print(result)
