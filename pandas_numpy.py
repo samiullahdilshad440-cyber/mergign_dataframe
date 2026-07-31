eeef276074284e2a6bf3dcd4ad6e77ce238b7d14
Sami Khan <samiullahdilshad440@gmail.com>
2026-07-31 19:40:27 +0500

    merge with two variables :

--- pandas_numpy.py
+++ pandas_numpy.py
@@ -1,92 +1,36 @@
 import pandas as pd
-
-df1 = pd.DataFrame({
-    'employee': ['Julia', 'Marie', 'Adam', 'Nicole'],
-    'department': ['Data Science', 'Web Development', 'Data Science', 'Cyber Security'],
-    'Year': ['2005', '2008', '2011', '2002']
+df4 = pd.DataFrame({
+    "employee": ["Julia", "Marie", "Adam", "Nicole"],
+    "department": ["Data Science", "Web Development", "Data Science", "Cyber Security"],
+    "salary": [20000, 17500, 16000, 22000]
 })
-df2 = pd.DataFrame({
-    'employee': ['Nicole', 'Adam', 'Julia', 'Marie'],
-    'country': ['Canada', 'England', 'USA', 'Germany'],
-    'salary': [22000, 16000, 20000, 17500]
-})
-df3 = pd.DataFrame({
-    'manager': ['Abraham', 'Joseph', 'Kayra'],
-    'department': ['Web Development', 'Cyber Security', 'Data Science']
-})
-
-result = pd.merge(df1, df2,)
-
-print("=" * 60)
-print("Inner Merge default")
-print("- Combines DataFrames using the common column.")
-print("- Returns only matching records from both DataFrames.")
-print("=" * 60)
-print(result)
-
-
-left = pd.merge(df1, df2, on="employee", how="left")
-print("=" * 60)
-print("Left Merge")
-print("- Keeps all rows from the left DataFrame (df1).")
-print("- Matching values are taken from the right DataFrame (df2).")
-print("=" * 60)
-print(left)
-
-right = pd.merge(df1, df2, on="employee", how="right")
-
-print("=" * 60)
-print("Right Merge")
-print("- Keeps all rows from the right DataFrame (df2).")
-print("- Matching values are taken from the left DataFrame (df1).")
-print("=" * 60)
-
-print(right)
-
-outer = pd.merge(df1, df2, on="employee", how="outer")
-print("=" * 60)
-print("Outer Merge")
-print("- Keeps all rows from both DataFrames.")
-print("- Missing matches are filled with NaN.")
-print("=" * 60)
-print(outer)
-
-df4 = pd.merge(df1, df2)
-print("=" * 70)
-print("Step 1 : Merge Employee Details")
-print("- Matching is performed using the 'employee' column.")
-print("=" * 70)
-print(df4)
 
 df6 = pd.DataFrame({
-    'department': [
-        'Web Development',
-        'Web Development',
-        'Cyber Security',
-        'Cyber Security',
-        'Data Science',
-        'Data Science'
+    "employee": [
+        "Julia",
+        "Julia",
+        "Adam",
+        "Adam",
+        "Marie",
+        "Nicole"
     ],
-    'prog_lang': [
-        'HTML',
-        'CSS',
-        'C++',
-        'SQL',
-        'PYTHON',
-        'R'
+    "department": [
+        "Data Science",
+        "Data Science",
+        "Data Science",
+        "Data Science",
+        "Web Development",
+        "Cyber Security"
+    ],
+    "prog_lang": [
+        "Python",
+        "R",
+        "Python",
+        "R",
+        "HTML",
+        "SQL"
     ]
 })
 
-print("=" * 70)
-print("Department and Programming Languages")
-print("- Each department can have multiple programming languages.")
-print("=" * 70)
-print(df6)
-
-result = pd.merge(df4, df6, on="department")
-print("=" * 70)
-print("Merge Employees with Programming Languages")
-print("- Matching is performed using the 'department' column.")
-print("- One department has multiple programming languages.")
-print("=" * 70)
-print(result)
+result1 = pd.merge(df4, df6, on=["employee", "department"])
+print(result1)
